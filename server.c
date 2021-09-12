6d2240b9ebfd6b7d771bafa7ea1cf3173cae1daf
Erik Priest <eriktpriest@gmail.com>
2021-09-12 14:55:28 +0200

    Update server.c

--- server.c
+++ server.c
@@ -1,31 +1,13 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<sys/types.h>
-#include<sys/socket.h>
-#include<netinet/in.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
 #include <unistd.h>
 
+// Note compile on the linux sever
 
-struct sockaddr{
-
-unsigned short sa_family; // address family AF_INET
-	char sa_sata[14]; // Family specific address information
-}
-
-struct in_addr{
-	unsigned long s_addr; // internet address 32 bits
-}
-
-struct sockaddr_in{
-	unsigned short sin_family; // address family AF_INET
-	unsigned short sin_port; // assign the port
-	struct in_addr sin_addr; // assign the ip address
-	char sin_ero[8];
-
-}
-
-
-int main()
+main()
 {
   int sockfd, clientfd;
   struct sockaddr_in serverAddr, cliAddr;
@@ -42,16 +24,16 @@ int main()
   serverAddr.sin_addr.s_addr = INADDR_ANY;
 
   // bind the address
-  bind(sockfd, (struct sockaddr *) &serverAddr, sizeof(serverAddr));
+  bind(sockfd, (struct sockaddr *)&serverAddr, sizeof(serverAddr));
 
   // listen
-  if(listen(sockfd,5)==0)
+  if (listen(sockfd, 5) == 0)
 
-  //accept the connection
-  clientfd = accept(sockfd, (struct sockaddr *) &cliAddr, &len);
+    //accept the connection
+    clientfd = accept(sockfd, (struct sockaddr *)&cliAddr, &len);
 
   // send the data
-  send(clientfd,serverMessage,sizeof(serverMessage),0);
+  send(clientfd, serverMessage, sizeof(serverMessage), 0);
 
   close(sockfd);
   return 0;
