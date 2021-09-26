09de428f31ebf426dd273b62dac8c1cc0f4b3b2c
Rishabh Narang <rishy2712@gmail.com>
2021-09-26 16:20:22 +0000

    print statements. And hash compare not working because of reverse order

--- server.c
+++ server.c
@@ -64,7 +64,7 @@ int main(int argc, char *argv[])
 
     // -1 is error message
     int count = recv(clientfd, &recievedMessage, sizeof(recievedMessage), MSG_WAITALL);
-    printf("\n %d\n", count);
+    printf("\n Bytes received count: %d\n", count);
     uint8_t hash_be[32];
     uint8_t q;
     uint64_t start,end;
@@ -73,19 +73,45 @@ int main(int argc, char *argv[])
     memcpy(&end,&(recievedMessage[40]),8);
     memcpy(&q,&(recievedMessage[48]),1);
     uint8_t hash_host[32];
+    printf("Printing the 8 bit numbers(hash sent by client) ... \n");
     for(int i =0; i< 32; i++){
 	hash_host[i] = hash_be[32-i-1];
+	printf("%" PRIu8 "",hash_host[i]);
+	printf("     ");
     }
+    printf("\nEnding Printing.. \n\n");
+    printf("\n Printing the binary version of each byte(hash sent by client)...");
+    for(int i =0; i< 32; i++){
+        bin(hash_host[i]);
+	printf("\n");
+    }
+    printf("The hash is %s", hash_host);
     start = be64toh(start);
     end = be64toh(end);
-    printf("%" PRIu64 "\n", start);
-    printf("%" PRIu64 "\n", end);
-    printf("%hhu\n", q);
+    printf("\n start value is %" PRIu64 "\n", start);
+    printf("end value is %" PRIu64 "\n", end);
+    printf("q value is %hhu\n", q);
     uint64_t result;
-    for(uint64_t test = 0; test < (start+end); test++){
+    for(uint64_t test = start; test < end; test++){
 	uint8_t hash_test[32];
-	SHA256((unsigned char *)test, 8, hash_test);
+
+	for(int i=0;i<32;i++){
+            printf("%" PRIu8 "   ", hash_test[i]);
+        }
+	printf("\n\n calculating the hash now for %" PRIu64 " \n",test);
+	SHA256(&test, 8, hash_test);
+	for(int i=0;i<32;i++){
+            printf("%" PRIu8 "   ", hash_test[i]);
+        }
+        
+	printf("\n Printing the binary version of each byte(hash test)...");
+        for(int i =0; i< 32; i++){
+          bin(hash_test[i]);
+          printf("\n");
+        }
+
 	int found = 0;
+	printf("Checking for equality of hashes... \n");
 	for(int i=0;i<32;i++){
 	    if(hash_test[i]!=hash_host[i]) break;
 	    else if (i==31) found = 1;
@@ -93,6 +119,7 @@ int main(int argc, char *argv[])
 	}
 	if(found==1){
 	    result = test;
+	    printf("Found the number %"PRIu64"\n", test);
 	    break;
 	}
     } 
@@ -103,6 +130,16 @@ int main(int argc, char *argv[])
 
   close(sockfd);
   return 0;
+}
+
+void bin(uint8_t n)
+{
+    /* step 1 */
+    if (n > 1)
+        bin(n / 2);
+ 
+    /* step 2 */
+    printf("%d", n % 2);
 }
 
 
