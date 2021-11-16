f2e7cecc4e4091ae41a05bfbdcb0ffdf6592a5d5
lfolsen <63238222+LFOlsen@users.noreply.github.com>
2021-11-16 13:18:46 +0100

    new linkedList.h

--- linkedList.h
+++ linkedList.h
@@ -1,4 +1,5 @@
 
+
 #include <stdio.h>
 #include <string.h>
 #include <inttypes.h>
@@ -11,12 +12,13 @@ typedef struct requestNode {
 	unsigned char hash[32];
 	uint64_t start, end;
 	uint8_t priority;
+	int clientfd;
 	struct requestNode* next, *prev;
 
 } requestNode;
 
 
-requestNode* getRequestNode(unsigned char hash[32], uint64_t start, uint64_t end, uint8_t priority) {
+requestNode* getRequestNode(unsigned char hash[32], uint64_t start, uint64_t end, uint8_t priority, int clientfd) {
 
 	// Create dynamic memory of LinkNode
 	struct requestNode* newReqNode = (requestNode*)malloc(sizeof(requestNode));
@@ -28,6 +30,7 @@ requestNode* getRequestNode(unsigned char hash[32], uint64_t start, uint64_t end
 	newReqNode->start = start;
 	newReqNode->end = end;
 	newReqNode->priority = priority;
+	newReqNode->clientfd = clientfd;
 
 	newReqNode->next = NULL;
 	newReqNode->prev = NULL;
@@ -40,77 +43,164 @@ typedef struct DoubleLinkedList {
 	// Define useful field of DoublyLinkedList
 	struct requestNode* head;
 	struct requestNode* tail;
+	int listSize;
+	
 
 }DoubleLinkedList;
+
 
 
 DoubleLinkedList* getDoubleLinkedList() {
 
 	// Create dynamic memory of DoublyLinkedList
-	DoubleLinkedList* linkedList = (DoubleLinkedList*)malloc(sizeof(DoubleLinkedList));
-	if (linkedList == NULL) {
+	DoubleLinkedList* dll = (DoubleLinkedList*)malloc(sizeof(DoubleLinkedList));
+	if (dll == NULL)
+	{
 		// Failed to create memory 
 		return NULL;
 	}
-	// Set head and tail
-	linkedList->head = NULL;
-	linkedList->tail = NULL;
-	return linkedList;
+	// Set head , tail and list size
+	dll->head = NULL;
+	dll->tail = NULL;
+	dll->listSize = 0;
+
+	return dll;
 }
 
 
 // Function: add new requestNode to end of linked list
-void insert(DoubleLinkedList* linkedList, unsigned char hash[32], uint64_t start, uint64_t end, uint8_t priority) {
+void insert(DoubleLinkedList* dll, unsigned char hash[32], uint64_t start, uint64_t end, uint8_t priority, int clientfd) {
 
-	// Create a new request node
-	requestNode* rn = getRequestNode(hash, start, end, priority);
-	if ((linkedList->head == NULL)) {
+	dll->listSize++;
+
+	// Create a new node
+	requestNode* newNode = getRequestNode(hash, start, end, priority, clientfd);
+
+	if ((dll->head == NULL))
+	{
 		// Add first node
-		linkedList->head = rn;
-		linkedList->tail = rn;
+		dll->head = newNode;
+		dll->tail = newNode;
 		return;
 	}
+	// Add node at last position
+	dll->tail->next = newNode;
+	newNode->prev = dll->tail;
+	dll->tail = newNode;
 
-	// Add node to end of linked list
-	linkedList->tail->next = rn;
-	rn->prev = linkedList->tail;
-	linkedList->tail = rn;
 }
 
 
-// Function: extract request with highest priority and remove it from linked list
-requestNode extractMax(DoubleLinkedList* linkedList) {
 
-	uint8_t max = linkedList->head->priority;
-	requestNode* nextNode, * prevNode;
-	nextNode = NULL;
-	prevNode = NULL;
+// Delete node value by priority value
+void deleteNode(DoubleLinkedList* dll, int p) {
+
+	requestNode* temp = NULL;
+	if ((dll->head == NULL)) {
+		// When linked list is empty
+		printf("Empty linked list");
+	}
+
+	// node to be deleted is head
+	if ((dll->head->priority == p)) {
+
+		// When remove head
+		temp = dll->head;
+		dll->head = dll->head->next;
+
+		if ((dll->head != NULL)) {
+			dll->head->prev = NULL;
+		}
+		else {
+			// When linked list empty after delete 
+			dll->tail = NULL;
+		}
+		free(temp);
+		return;
+	}
+
+	// node to be deleted is tail
+	else if ((dll->tail->priority == p)) {
+
+		// When remove last node
+		temp = dll->tail;
+		dll->tail = dll->tail->prev;
+
+		if ((dll->tail != NULL)) {
+			dll->tail->next = NULL;
+		}
+		else {
+			// Remove all nodes
+			dll->head = NULL;
+		}
+		free(temp);
+		return;
+	}
+
+
+	// node to be deleted is neither head or tail in list
+	else {
+
+		temp = dll->head;
+
+		while (temp != NULL && temp->priority != p) {
+			temp = temp->next;
+		}
+		if ((temp == NULL)) {
+
+			// Node key not exist
+			printf("Deleted node are not found");
+			return;
+		}
+		else {
+
+			// Separating deleted node And combine next and previous node
+			temp->prev->next = temp->next;
+			if ((temp->next != NULL)) {
+				// When deleted intermediate nodes
+				temp->next->prev = temp->prev;
+			}
+
+			free(temp);
+			return;
+		}
+	}
+}
+
+
+
+
+// Function: extract request with highest priority and remove it from linked list
+requestNode extractMax(DoubleLinkedList* dll) {
+
+
+	uint8_t max = dll->head->priority;
+	(dll->listSize)--;
 
 	// Get first node of linked list
-	requestNode* temp = linkedList->head;
+	requestNode* temp = dll->head;
 
 	requestNode maxPriorityRequest = *temp;
+
 
 	// iterate linked list 
 	while (temp != NULL) {
 		if (temp->priority > max) {
 			max = temp->priority;
 			maxPriorityRequest = *temp;
-			prevNode = temp->prev;
-			nextNode = temp->next;
-
 		}
-		// Go to next request node
+		// Visit to next node
 		temp = temp->next;
 	}
-	prevNode->next = nextNode;
-	nextNode->prev = prevNode;
+
+
+	deleteNode(dll, max);
+
 
 	return maxPriorityRequest;
 }
 
 
-/* Print linked list
 void printLinkedList(struct requestNode* nodePtr) {
 
 	printf("Doubly Linked List\n");
@@ -120,4 +210,4 @@ void printLinkedList(struct requestNode* nodePtr) {
 		if (nodePtr != NULL)
 			printf("-><-");
 	}
-} */
+} 
