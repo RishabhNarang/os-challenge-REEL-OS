9e36cd8f547e9bdc78a006076e8c80a6135c4b92
EliasAdawi <eliasadawi@gmail.com>
2021-11-17 17:25:44 +0100

    Update Dictionnnary test.c
    fixed dictionary

--- Dictionnnary test.c
+++ Dictionnnary test.c
@@ -1,70 +1,63 @@
-#define NULL 0
+#include <inttypes.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 struct nlist { /* table entry: */
     struct nlist *next; /* next entry in chain */
-    char *name; /* defined name */
-    char *defn; /* replacement text */
+    unsigned char hash[32]; /* defined name */
+    uint64_t result; /* replacement text */
 };
 
 #define HASHSIZE 1001
-static struct nlist *hashtab[HASHSIZE]; /* pointer table */
+//static struct nlist *hashtab[HASHSIZE];
 
 /* hash: form hash value for string s */
-unsigned hash(char *s)
+unsigned hash_fn(unsigned char s[])
 {
-    unsigned hashval;
-    for (hashval = 0; *s != '\0'; s++)
-      hashval = *s + 31 * hashval;
+    unsigned hashval = 0;
+    for (int i = 0; i != 32; i++)
+      hashval = s[i] + 31 * hashval;
     return hashval % HASHSIZE;
 }
 
 /* lookup: look for s in hashtab */
-struct nlist *lookup(char *s)
+struct nlist *lookup(struct nlist * hashtab[], unsigned char s[])
 {
     struct nlist *np;
-    for (np = hashtab[hash(s)]; np != NULL; np = np->next)
-        if (strcmp(s, np->name) == 0)
+    for (np = hashtab[hash_fn(s)]; np != 0; np = np->next)
+        if (memcmp(s, np->hash,32) == 0)
           return np; /* found */
-    return NULL; /* not found */
+    return 0; /* not found */
 }
 
-char *strdup(char *);
 /* install: put (name, defn) in hashtab */
-struct nlist *install(char *name, char *defn)
+int install(struct nlist * hashtab[], unsigned char key[], uint64_t val)
 {
     struct nlist *np;
     unsigned hashval;
-    if ((np = lookup(name)) == NULL) { /* not found */
-        np = (struct nlist *) malloc(sizeof(*np));
-        if (np == NULL || (np->name = strdup(name)) == NULL)
-          return NULL;
-        hashval = hash(name);
+    if ((np = lookup(hashtab,key)) == 0) { /* not found */
+        np = (struct nlist *) malloc(sizeof(struct nlist));
+  	np->result = val;
+	memcpy(np->hash,key,32);
+        hashval = hash_fn(key);
         np->next = hashtab[hashval];
         hashtab[hashval] = np;
     } else /* already there */
-        free((void *) np->defn); /*free previous defn */
-    if ((np->defn = strdup(defn)) == NULL)
-       return NULL;
-    return np;
+        return 1;
+    return 0;
 }
-
-char *strdup(char *s) /* make a duplicate of s */
-{
-    char *p;
-    p = (char *) malloc(strlen(s)+1); /* +1 for ’\0’ */
-    if (p != NULL)
-       strcpy(p, s);
-    return p;
-}
-
 /*
 void main(){
-    unsigned char name[] = "test";
-    int x = 20;
-    char defn[] = "test1";
-    char *val = &x +'0';
-    install(name,val);
-    struct nlist *res= lookup("test");
-    printf("%s %s",res->name,res->defn);
-}
-*/
+    unsigned char key1[32] = "test1";
+    unsigned char key2[32] = "test2";
+    uint64_t result1 = 20,result2 = 30;
+    install(key1,result1);
+    install(key2,result2);
+    unsigned char key1lookup[32] = "test1";
+    unsigned char key2lookup[32] = "test2";
+    struct nlist* x1 = lookup(key1lookup);
+    struct nlist* x2 = lookup("test3");
+    if(x2==NULL)
+	    printf("Result2: not found\n");
+    printf("resul1: %" PRIu64 "\n", x1->result);
+}*/
